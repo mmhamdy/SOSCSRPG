8193f9a29528c054891a21d29905bf42f0fa7288
mmhamdy <mohamdy2772@gmail.com>
2021-07-29 23:01:28 +0200

    Adding Location Image

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -26,10 +26,7 @@ namespace Engine.ViewModels
             CurrentLocation.XCoordinate = 0;
             CurrentLocation.YCoordinate = -1;
             CurrentLocation.Description = "This is your house";
+            CurrentLocation.ImageName = "pack://application:,,,/Engine;component/Images/Locations/Home.png";
         }
-        
-
-
-
     }
 }
