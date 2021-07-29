b9a6c362cff69e33875ce6c285bcded7c7e71243
mmhamdy <mohamdy2772@gmail.com>
2021-07-29 22:05:06 +0200

    Creating Location class

---
+++ Engine/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class Location
+    {
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string ImageName { get; set; }
+    }
+}
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -9,6 +9,7 @@ namespace Engine.ViewModels
     {
         // use Player as the datatype for the CurrentPlayer property
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
 
         public GameSession()
         {
@@ -20,6 +21,11 @@ namespace Engine.ViewModels
             CurrentPlayer.Level = 1;
             CurrentPlayer.Gold = 1000000;
 
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your house";
         }
         
 
